1577dfc15eddad2cd30b6865c846ef8ef846028b
Lê Đình-Bảo <ledinhbao@me.com>
2020-03-09 11:52:57 +0700

    Add: UpdateStringForNamedQuery trả về query để save model vào database.

--- customer.go
+++ customer.go
@@ -8,3 +8,15 @@ type NGCCustomer struct {
 	CreatedAt time.Time `db:"created_at"`
 	UpdatedAt time.Time `db:"updated_at"`
 }
+
+// UpdateStringForNamedQuery return insert query string if NGCCustomer.id != 0
+func (x *NGCCustomer) UpdateStringForNamedQuery() string {
+	queryString := ""
+	if x.Id != 0 {
+		queryString = "UPDATE customers SET name = :name, updated_at = date(\"now\") WHERE id = :id"
+	} else {
+		queryString = "INSERT INTO customers (name, created_at, updated_at) " +
+			"VALUES (:name, :created_at, :updated_at)"
+	}
+	return queryString
+}
--- main.go
+++ main.go
@@ -6,6 +6,15 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+func saveNGCCustomerToDatabase(db *sqlx.DB, entity NGCCustomer) {
+	tx := db.MustBegin()
+	_, b := tx.NamedExec(entity.UpdateStringForNamedQuery(), entity)
+	if b != nil {
+		fmt.Println(b)
+	}
+	_ = tx.Commit()
+}
+
 func main() {
 	//db, err := sql.Open("sqlite3", "./database.db")
 	db, err := sqlx.Connect("sqlite3", "./database.db")
@@ -13,12 +22,22 @@ func main() {
 		panic(err)
 	}
 
+	//sampleCustomer := NGCCustomer{
+	//	Id:        6,
+	//	Name:      "Le Thanh Tra",
+	//	CreatedAt: time.Now(),
+	//	UpdatedAt: time.Now(),
+	//}
+	//saveNGCCustomerToDatabase(db, sampleCustomer)
+
 	var customers []NGCCustomer
 	err = db.Select(&customers, "SELECT * FROM customers")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(customers)
+	for _, item := range customers {
+		fmt.Println(item)
+	}
 
 	fmt.Print("Hello World")
 }
