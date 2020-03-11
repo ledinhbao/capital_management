57d04f0b024d6cfdc14cac5c9d62e5136860d7e2
Lê Đình-Bảo <ledinhbao@me.com>
2020-03-11 13:36:11 +0700

    Create capitalManager

---
+++ capital/capital_manager.go
@@ -0,0 +1,15 @@
+package capital
+
+type Manager struct {
+
+}
+
+func (m *Manager) CreateCapitalTransaction() {
+
+}
+
+/*
+What does Manager do?
+	- Broadcast changes.
+	- Changes: has new transaction, state changed,
+ */
